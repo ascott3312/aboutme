ef12e1eebaf6916a7dbc0ede55e43b9495a9479f
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-16 18:14:40 -0500

    Added in the five questions

--- js/app.js
+++ js/app.js
@@ -8,13 +8,48 @@ var firstName = prompt('What is your first name?');
 console.log('First Name is' + firstName);
 
 //Initial Greeting
-alert('Welcome' + firstName + ' to my website. Thanks for viewing!!');
+alert('Welcome' +' '+ firstName + ' to my website. Thanks for viewing!!');
 
-//Questions
+//Question 1
 var question1 = prompt (' '+ firstName +' is this the first time visiting this website? Yes or No.').toLowerCase().trim();
 console.log(question1);
 if (question1 ==='yes') {
-  alert('Well thank you for visiting');
+  alert('Well thank you for visiting.');
 } else if (question1 !== 'yes' || question1 === isNaN){
-  alert ('Please answer with either Yes or No');
+  alert ('Please answer with either Yes or No')
 }
+//Question 2
+var question2 = prompt ('Do you know where I live? Yes or No.').toLowerCase().trim();
+console.log(question2);
+if (question2 ==='yes') {
+  alert('Great I love living in Iowa.');
+} else if (question2 !== 'yes' || question2 === isNaN){
+  alert ('Please answer with either Yes or No')
+}
+//Question 3
+var question3 = prompt ('Do you live in the same state as I do? Yes or No.').toLowerCase().trim();
+console.log(question3);
+if (question3 ==='yes') {
+  alert('Awesome.');
+} else if (question3 !== 'yes' || question3 === isNaN){
+  alert ('Please answer with either Yes or No')
+}
+//Question 4
+var question4 = prompt ('Do you have any kids? Yes or No.').toLowerCase().trim();
+console.log(question4);
+if (question4 ==='yes') {
+  alert('I have 4 kids.');
+} else if (question4 !== 'yes' || question4 === isNaN){
+  alert ('Please answer with either Yes or No')
+}
+//Question 5
+var question5 = prompt ('Do you like the game of football? Yes or No.').toLowerCase().trim();
+console.log(question5);
+if (question5 ==='yes') {
+  alert('Awesome! I love football too.');
+} else if (question5 !== 'yes' || question5 === isNaN){
+  alert ('Please answer with either Yes or No')
+}
+
+
+
