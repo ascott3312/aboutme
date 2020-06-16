5bf2aca39972a7faef6f7b69d2763ad6c056aaf2
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-16 16:21:30 -0500

    Adding first requiremnts to js file

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
+// Your JS file must contain a 'use strict' declaration at the top, and your javascript file must be linked in your html file as an external script file.
 'use strict';
+ console.log('This file is connected.');
 
+ 
+    //First Question
+    var firstName = prompt('What is your first name?');
+    console.log('First Name is'  + firstName);
+    
+    //Initial Greeting
+    alert('Welcome ' + firstName + ' ' + 'to my website. Thanks for viewing!!');
+
+    // alert('Welcome ' + firstName + ' ' + lastName + ', ' + 'your favorite sport is, ' + sport + ' and your favorite team is the ' + team + '.');
