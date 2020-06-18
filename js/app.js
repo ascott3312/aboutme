bae9dc451bde01c0162493101c4a16feea68cd22
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-18 10:22:28 -0500

    Question 6 Updates

--- js/app.js
+++ js/app.js
@@ -56,20 +56,20 @@ if (question5 ==='yes') {
   alert ('Please answer with either Yes or No')
 }
 //Question 6
-var winNumber = 30; //Nunber they need to guess
+var winNumber = 30; //Nunber they need to guess my favorite number
 for (var i = 0; i <= 8; i++){
 var question6 = prompt ('Let\'s see if you can guess my favorite number?. Enter in a number between 1 and 100');
 console.log(question6);
-if (question6 == winNumber) {
+if (question6 === winNumber) {
   alert('Awesome. This ' + question6 + ' is my favorite number');(i === 8);break;
 } 
 if (question6 < winNumber) {
   alert('The number you guessed is lower than my favorite number');(i++);continue;
-} if(question6 > winNumber) {;
+} if(question6 > winNumber) {
   alert('The number you guessed is greater than my favorite number');(i++);continue;
 }
- if(question6 === null || question6 === ' ')
- alert('Please enter in a number.');(i++);continue;
+//  if(question6 === null || question6 === ' ')
+//  alert('Please enter in a number.');(i++);continue;
 }
 console.log('the variable i',i )
 
