048b2ad8074f2f78433478ee6037e85720432610
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-19 13:28:18 -0500

    Added in the no and n fuctionality

--- js/app.js
+++ js/app.js
@@ -21,56 +21,76 @@ function questionOne(){
   // console.log('Function call');
   var question1 = prompt(firstName + ' is this the first time visiting this website? Yes or No.').toLowerCase().trim();
   console.log(question1);
-  if (question1 === 'yes') {
+  if (question1 === 'yes' || question1 === 'y') {
     alert('Well thank you for visiting.');
     userPoints += 1;
   }
-  else if (question1 !== 'yes' || question1 === isNaN){
-    alert ('Please answer with either Yes or No');
+  else if (question1 === 'no' || question1 ==='n'){
+    alert('Great! Welcome back. ' + firstName + '.');
+  }
+  else {
+    alert ('You did not enter in Yes or No');
+    questionOne();
   }
 }
 //Question 2
 function questionTwo(){
   var question2 = prompt ('Do you know where I live? Yes or No.').toLowerCase().trim();
   console.log(question2);
-  if (question2 ==='yes') {
+  if (question2 ==='yes' || question2 === 'y') {
     alert('Great I love living in Iowa.');
     userPoints += 1;
-  } else if (question2 !== 'yes' || question2 === isNaN){
-    alert ('Please answer with either Yes or No');
+  } else if (question2 === 'no' || question2 === 'n'){
+    alert ('I live in Iowa');
+  }
+  else {
+    alert ('You did not enter in Yes or No');
+    questionTwo();
   }
 }
 //Question 3
 function questionThree(){
   var question3 = prompt ('Do you live in the same state as I do? Yes or No.').toLowerCase().trim();
   console.log(question3);
-  if (question3 ==='yes') {
+  if (question3 ==='yes' || question3 === 'y') {
     alert('Awesome.');
     userPoints += 1;
-  } else if (question3 !== 'yes' || question3 === isNaN){
-    alert ('Please answer with either Yes or No');
+  } else if (question3 === 'no' || question3 === 'n'){
+    alert ('So you live in a different state');
+  }
+  else {
+    alert ('You did not enter in Yes or No');
+    questionThree();
   }
 }
 //Question 4
 function questionFour(){
   var question4 = prompt ('Do you have any kids? Yes or No.').toLowerCase().trim();
   console.log(question4);
-  if (question4 ==='yes') {
+  if (question4 ==='yes' || question4 === 'y') {
     alert('I have 4 kids.');
     userPoints += 1;
-  } else if (question4 !== 'yes' || question4 === isNaN){
-    alert ('Please answer with either Yes or No');
+  } else if (question4 === 'no' || question4 === 'n'){
+    alert ('So you have no kids');
+  }
+  else {
+    alert ('You did not enter in Yes or No');
+    questionFour();
   }
 }
 //Question 5
 function questionFive(){
   var question5 = prompt ('Do you like the game of football? Yes or No.').toLowerCase().trim();
   console.log(question5);
-  if (question5 ==='yes') {
+  if (question5 ==='yes' || question5 === 'y') {
     alert('Awesome! ' + firstName + ' I love football too.');
     userPoints += 1;
-  } else if (question5 !== 'yes' || question5 === isNaN){
-    alert ('Please answer with either Yes or No');
+  } else if (question5 === 'no' || question5 === 'n'){
+    alert ('Not every one likes football');
+  }
+  else {
+    alert ('You did not enter in Yes or No');
+    questionFive();
   }
 }
 //Question 6
